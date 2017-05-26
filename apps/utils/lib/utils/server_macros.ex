f0536c3b9c698fc2a140c4d99f6e848238f10221
Aldric Giacomoni <trevoke@gmail.com>
2017-05-26 19:33:38 -0400

    better macro

--- apps/utils/lib/utils/server_macros.ex
+++ apps/utils/lib/utils/server_macros.ex
@@ -1,8 +1,28 @@
 defmodule Utils.ServerMacros do
+  @moduledoc """
+  Macros for automatic API / callback generation
+  for genservers
+  """
+
+  # TODO - macro with no payload
+
+  # TODO - use via_tuple
+  # TODO - tell people that without a via_tuple
+  # life is gonna suck
+  #
+  # TODO - what about other returns like :stop?
+  #
+  # TODO - defcall
+
   defmacro defcast(message, payload, do: block) do
     quote do
+
+      def unquote(message)(payload) do
+        GenServer.cast(__MODULE__, {unquote(message), unquote(payload)})
+      end
+
       def handle_cast({message, payload}, state) do
-        Kernel.apply(__MODULE__, unquote(message), [unquote(payload)])
+        Kernel.apply(__MODULE__, :"do_#{unquote(message)}", [unquote(payload)])
         unquote(block)
         {:noreply, state}
       end
