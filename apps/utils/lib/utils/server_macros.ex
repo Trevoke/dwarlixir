8ae78c8cf29bae1c38f64d88fd696c4847f38077
Aldric Giacomoni <trevoke@gmail.com>
2017-05-27 09:03:00 -0400

    more thoughts on macro

--- apps/utils/lib/utils/server_macros.ex
+++ apps/utils/lib/utils/server_macros.ex
@@ -4,8 +4,15 @@ defmodule Utils.ServerMacros do
   for genservers
   """
 
-  # TODO - macro with no payload
 
+  defmacro __using__(_opts) do
+    quote do
+      import Utils.ServerMacros
+    end
+  end
+
+  # TODO - macro with no payload
+  # TODO - more than one argument
   # TODO - use via_tuple
   # TODO - tell people that without a via_tuple
   # life is gonna suck
@@ -16,16 +23,38 @@ defmodule Utils.ServerMacros do
   #
   # TODO - document the "do_foo"
 
-  defmacro defcast(message, payload, do: block) do
+  @doc """
+  REQUIRES a `via_tuple/1` function to be defined.
+  REQUIRES a `do_foo` function to be defined with the correct arity.
+
+  Defines an API function and a related async callback:
+
+  defcast(:arrive, loc_id, mob) do
+    # stuff
+  end
+
+  will create:
+
+  def arrive(loc_id, mob) do
+    GenServer.cast(via_tuple(loc_id), {:arrive, mob})
+  end
+
+  def handle_cast({:arrive, mob}, state) do
+    new_state = do_arrive(mob, state)
+    # stuff
+    {:noreply, new_state}
+  end
+  """
+  defmacro defcast(message, id, payload, do: block) do
     quote do
 
       def unquote(message)(payload) do
-        GenServer.cast(__MODULE__, {unquote(message), unquote(payload)})
+        GenServer.cast(via_tuple(unquote(id)), {unquote(message), unquote(payload)})
       end
 
       def handle_cast({message, payload}, state) do
-        Kernel.apply(__MODULE__, :"do_#{unquote(message)}", [unquote(payload)])
         unquote(block)
+        state = Kernel.apply(__MODULE__, :"do_#{unquote(message)}", [unquote(payload), state])
         {:noreply, state}
       end
     end
