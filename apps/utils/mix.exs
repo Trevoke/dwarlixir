0b59468f176777fa809537b3d29ce8794956054b
Aldric Giacomoni <trevoke@gmail.com>
2017-08-12 17:01:06 -0400

    elixir 1.5.1, some changes need to be made still

--- apps/utils/mix.exs
+++ apps/utils/mix.exs
@@ -4,7 +4,7 @@ defmodule Utils.Mixfile do
   def project do
     [app: :utils,
      version: "0.1.0",
-     elixir: "~> 1.4",
+     elixir: "~> 1.5",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
