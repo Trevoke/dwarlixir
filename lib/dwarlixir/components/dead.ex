3715a6a60b780ed72bfa5dc1d973e40472ff797f
Aldric Giacomoni <trevoke@gmail.com>
2017-12-22 22:31:58 -0500

    components use module attributes

--- lib/dwarlixir/components/dead.ex
+++ lib/dwarlixir/components/dead.ex
@@ -1,4 +1,3 @@
 defmodule Dwarlixir.Components.Dead do
   use Ecstatic.Component
-  def default_value, do: %{}
 end
--- lib/dwarlixir/components/mortal.ex
+++ lib/dwarlixir/components/mortal.ex
@@ -1,4 +1,4 @@
 defmodule Dwarlixir.Components.Mortal do
   use Ecstatic.Component
-  def default_value, do: %{mortal: true}
+  @default_value %{mortal: true}
 end
